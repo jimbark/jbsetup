50c2bd127ef33b4c1a3522f04ea5f4ac6d9c128f
Jim Bark <jimbark007@gmail.com>
2014-06-21 10:00:44 +0000

    corrected nvm URL and commands and sudo jshint

--- setup.sh
+++ setup.sh
@@ -6,16 +6,15 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
+curl https://raw.githubusercontent.com/creationix/nvm/v0.8.0/install.sh | sh
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10
+nvm use v0.10
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
-npm install -g jshint
+sudo npm install -g jshint
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
